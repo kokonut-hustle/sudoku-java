acc70491e19576c15b7484909a7133c75590aaf7
trungdd <kokonut.hustle@gmail.com>
2022-10-14 23:56:25 +0700

    update container

--- Container.java
+++ Container.java
@@ -9,5 +9,9 @@ public class Container {
         time_lb = new JLabel("00:00:00:00");
         sls_lb = new JLabel("3 times remaining");
 
+        newGame_bt = new JButton("New Game");
+        newGame_bt.addActionListener(this);
+        highScore_bt = new JButton("High Score");
+        highScore_bt.addActionListener(this);
     }
 }
